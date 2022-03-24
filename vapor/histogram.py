04d3919dfe49f61db98bf563f92bf7e62743785a
tobiaszpuslecki <tobi0405@icloud.com>
2022-03-24 23:06:18 +0100

    histograms generator enhanced

--- vapor/histogram.py
+++ vapor/histogram.py
@@ -3,34 +3,42 @@ from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
 import os
 
-dir_text = 'BBC News Summary/News Articles/tech/'
-dir_summ = 'BBC News Summary/Summaries/tech/'
+subjects = ['tech', 'sport', 'politics', 'entertainment', 'business']
 
-arr = os.listdir(dir_text)
+for subject in subjects:
 
-counts = []
-ir=[]
+    dir_text = 'BBC News Summary/News Articles/' + subject + '/'
+    dir_summ = 'BBC News Summary/Summaries/' + subject + '/'
 
-for news in arr:
-    with open(dir_text + news) as f:
-        text = f.read()
-    with open(dir_summ + news) as f:
-        summ = f.read()
-    
-    summ = summ.replace('.', '. ')
+    arr = os.listdir(dir_text)
 
-    sentence_list_text = nltk.sent_tokenize(text)
-    sentence_list_summ = nltk.sent_tokenize(summ)
+    counts = []
+    ir=[]
 
-    counts.append(len(sentence_list_text))
-    ir.append(len(sentence_list_summ)/len(sentence_list_text))
+    for news in arr:
+        with open(dir_text + news) as f:
+            # News Articles/sport/199.txt - remove pound symbol
+            text = f.read()
+        with open(dir_summ + news) as f:
+            summ = f.read()
 
-fig, ax = plt.subplots(2,1,figsize=(6,10))
+        summ = summ.replace('.', '. ')
 
-ax[0].hist(counts, bins=50)
-ax[0].set_title('sentence num')
+        sentence_list_text = nltk.sent_tokenize(text)
+        sentence_list_summ = nltk.sent_tokenize(summ)
 
-ax[1].hist(ir, bins=50)
-ax[1].set_title('IR')
-plt.savefig('foo.png')
+        counts.append(len(sentence_list_text))
+        ir.append(len(sentence_list_summ)/len(sentence_list_text))
 
+    fig, ax = plt.subplots(2,1,figsize=(6,10))
+
+    ax[0].hist(counts, bins=50)
+    ax[0].set_title(subject + ' - articles lengths')
+    ax[0].set_xlabel("Article length")
+    ax[0].set_ylabel("Number of articles")
+
+    ax[1].hist(ir, bins=50)
+    ax[1].set_title(subject + ' - sentences percentage in summaries')
+    ax[1].set_xlabel("sentences percentage in summaries")
+    ax[1].set_ylabel("Number of summaries")
+    plt.savefig(subject + '.png')
