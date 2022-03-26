e189a3169f3dadfed481a08d4f583f3e4ad8fa83
Piotr Karon <piotr.karon@millionpugs.com>
2022-03-26 23:29:20 +0100

    Cleanup rubbish before and after sentence

--- data_prep.py
+++ data_prep.py
@@ -2,51 +2,52 @@ import os
 import pandas as pd
 import re
 
-total = [0,0,0,0,0]
+total = [0, 0, 0, 0, 0]
 
 dirs = ['tech/', 'sport/', 'politics/', 'entertainment/', 'business/']
 dir_text = 'BBC News Summary/News Articles/'
 dir_summ = 'BBC News Summary/Summaries/'
 
 for d_i, d in enumerate(dirs):
-    text_files = "%s%s" % (dir_text,d)
-    summ_files = "%s%s" % (dir_summ,d)
+    text_files = "%s%s" % (dir_text, d)
+    summ_files = "%s%s" % (dir_summ, d)
 
     arr = os.listdir(text_files)
     for news in arr:
         with open(text_files + news) as f:
-            title = f.readline()
-            text = f.read()
+            try:
+                title = f.readline()
+                text = f.read()
+            except Exception:
+                print("Error", f.name)
+
         with open(summ_files + news) as f:
             summ = f.read()
 
-        summ = re.sub("\"|-","",summ)
-        text = re.sub("\"|-","",text)
+        summ = re.sub("\"|-|'|:", "", summ)
+        summ = re.sub("[,]", " ", summ)
+        text = re.sub("\"|-|'|:", "", text)
+        text = re.sub("[,]", " ", text)
 
-        summ = re.sub("\s\s"," ",summ)
-        text = re.sub("\s\s"," ",text)
+        summ = re.sub("\s\s", " ", summ)
+        text = re.sub("\s\s", " ", text)
 
-        sentence_list_summ = re.split('(?=\.[A-Z])\.',summ)
-        sentence_list_text = re.split('\.(?<=\.)[\s\n]',text)
+        sentence_list_summ = re.split('.+(?=[\.\?\!]\s?[A-Z])', summ)
+        sentence_list_text = re.split('\.(?<=[\.\?\!])[\s\n]', text)
 
         sentence_list_text_cln = []
         sentence_list_summ_cln = []
 
         for t in sentence_list_text:
-            t = re.sub('\n', '', t)
-            t = t.strip()
-            if t=='':
+            t = re.sub('^[\W](.*)[\W]$', '', t)
+            if t == '':
                 continue
-            if t[-1]=='.':
-                t=t[:-1]
             sentence_list_text_cln.append(t)
+
         for s in sentence_list_summ:
-            s = re.sub('\n', '', s)
-            s = s.strip()
-            if s=='':
+            s = re.sub('^[\W](.*)[\W]$', '', s)
+            if s == '':
                 continue
-            if s[-1]=='.':
-                s=s[:-1]
             sentence_list_summ_cln.append(s)
 
         err = False
@@ -57,20 +58,20 @@ for d_i, d in enumerate(dirs):
                 err = True
         if err:
             continue
-                
-        data=[]
+
+        data = []
         for sent_text in sentence_list_text_cln:
             if sent_text in sentence_list_summ_cln:
-                label=1
+                label = 1
             else:
-                label=0
-            
+                label = 0
+
             data.append([sent_text, label])
-        
-        df = pd.DataFrame(data, columns = ['Sentence', 'Label'])
-        df.to_csv('prep/%s%s'% (d, news)) 
+
+        df = pd.DataFrame(data, columns=['Sentence', 'Label'])
+        df.to_csv('prep/%s%s' % (d, news))
         total[d_i] += 1
 
         # print(sentence_list_text_cln)
         # exit()
-print(total)    
+print(total)
