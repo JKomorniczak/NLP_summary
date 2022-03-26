013b43e71cd115a491b2db7d3a9e07ee40a5c2ab
Joana <asiakom24@vp.pl>
2022-03-26 11:23:01 +0100

    fix histogram

--- histogram.py
+++ histogram.py
@@ -6,9 +6,11 @@ import pandas as pd
 import numpy as np
 subjects = ['tech', 'sport', 'politics', 'entertainment', 'business']
 
-fig, ax = plt.subplots(5,2,figsize=(7,10))
-ax[0,0].set_title('Article length in sentences')
-ax[0,1].set_title('Sentences fraction in summaries')
+fig, ax = plt.subplots(5,2,figsize=(8,10), sharex='col', )
+ax[0,0].set_title('Article length')
+ax[0,1].set_title('Summary length in %')
+ax[-1,0].set_xlabel('sentences')
+ax[-1,1].set_xlabel('% of sentences')
 
 for s_id, subject in enumerate(subjects):
 
@@ -26,14 +28,18 @@ for s_id, subject in enumerate(subjects):
         c = len(data.index)
         counts.append(c)
 
-        i = np.sum(data.Label == 1)/c
+        i = 100*np.sum(data.Label == 1)/c
         ir.append(i)
 
     ax[s_id,0].hist(counts, bins=50)
     ax[s_id,0].set_xlim(0, 80)
+    ax[s_id,0].spines[['top', 'right']].set_visible(False)
+
 
     ax[s_id,1].hist(ir, bins=50)
-    ax[s_id,1].set_xlim(0.35, 0.55)
+    ax[s_id,1].set_xlim(35, 55)
+    ax[s_id,1].spines[['top', 'right']].set_visible(False)
+
 
     ax[s_id,0].grid()
     ax[s_id,1].grid()
