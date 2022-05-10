874806158b8ea95f6316165d350377ecc33f2049
Joana <asiakom24@vp.pl>
2022-05-10 23:13:57 +0200

    plt

--- exp_news_analyse_h.py
+++ exp_news_analyse_h.py
@@ -6,22 +6,27 @@ extractors = ['KBest-5', 'KBest-15', 'KBest-25', 'PCA-5', 'PCA-15', 'PCA-25']
 clfs = ['GNB', 'KNN', 'MLP', 'DT']
 vectorizers = ['CV', 'TF', 'TFIDF']
 
-fig, ax = plt.subplots(5, 3, figsize=(10,15), sharex=True, sharey=True)
+fig, ax = plt.subplots(5, 3, figsize=(10,13), sharex=True, sharey=True)
 
 # collect
 for d_id, dir in enumerate(dirs):
-    res = np.load('res_news_%i.npy' % d_id)[0]
+    res = np.load('res/res_news_%i.npy' % d_id)[0]
     # print(res.shape) # vactorizers, extractors, clfs, news
     for v_id, vect in enumerate(vectorizers):
         r_mean = np.mean(res[v_id], axis=2)
 
         axx = ax[d_id, v_id]
         axx.imshow(r_mean.T, vmax=1, vmin=0.5)
-        axx.set_title(dir+' '+vect)
+        # axx.set_title(dir+' '+vect)
         axx.set_xticks(range(len(extractors)))
         axx.set_xticklabels(extractors, rotation=90)
         axx.set_yticks(range(len(clfs)))
         axx.set_yticklabels(clfs)
+
+        if v_id==0:
+            axx.set_ylabel(dir)
+        if d_id==0:
+            axx.set_title(vect)
 
         for _a, __a in enumerate(extractors):
             for _b, __b in enumerate(clfs):
